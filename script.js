df33402dce1dbfb9d13c34b9849a547d49143450
MrChae1 <ljnellam27@gmail.com>
2023-11-19 22:30:58 +0700

    move function with api keys

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
+import { getResponse, getResponse } from "./api";
 let defaultLocation = 'bulacan';
 let check = false;
-let isLoading = true;
 const mainTag = document.querySelector('.document-main');
 const sectionTag = mainTag.querySelector('.main-section');
 const modalTag = mainTag.querySelector('.mainModal');
@@ -10,6 +10,8 @@ const AllHour = Array.from(document.querySelectorAll('.hour-desc'));
 const InputTag = document.querySelector('.input-nav');
 const inputBtn = document.querySelector('.find-btn');
 const changeClick = document.querySelector('.Unit-Temp'); 
+
+
 changeClick.addEventListener('click', () => {
     check = check === false ? true : false;
     getData().then((res) => {
@@ -23,30 +25,10 @@ inputBtn.addEventListener('click', () => {
     displayAll();
 });
 
-const getResponse = async (location) => {
-    const maxRetries = 3;
-    let currentRetry = 0;
-    while (currentRetry < maxRetries) {
-        try {
-            const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=a733914831544ebe96e154016230311&q=${location}&days=3&aqi=no&alerts=no`);
-            const convert = await response.json();
-            isLoading = false; // Set loading to false on successful fetch
-            loadingResponse();
-            return convert;
-        } catch (error) {
-            console.error("Error in getResponse:", error);
-            currentRetry++;
-        }
-    }
 
-    // If all retries fail, you might want to handle it here
-    console.error("Max retries reached. Unable to fetch data.");
-    isLoading = false; // Set loading to false if max retries are reached without success
-    return notFetch(); // or throw an error, or return a default value
-};
 
 const getData = async () => {
-    const response = await getResponse(defaultLocation);
+    const response = await getResponse(defaultLocation, isLoading);
     return response;
 }
 
@@ -94,15 +76,7 @@ function verifyCheck(res){
     }
 }
 
-function loadingResponse(){
-    if(isLoading === true){
-        displayHidden();
-    }
-    else{
-        displayMain();
-        isLoading = true;
-    }
-}
+
 
 function displayAllcelcius(response, temp, feels){
     temp.innerHTML = `${response.current.temp_c}°C`; //h2
